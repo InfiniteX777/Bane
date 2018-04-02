be1a9750b562190eeebd4de99f78daeb6b95b456
InfiniteX777 <InfiniteX777@gmail.com>
2018-04-02 20:17:43 +0800

    Fix exploit.
    'this' class can no longer be instantiated.
    Renamed argument 'rect' to 'size' from '.wrap' function.

--- asset/api/SenPy/kuudere.py
+++ asset/api/SenPy/kuudere.py
@@ -20,7 +20,7 @@
 
 	.wrap(
 		font,
-		rect,
+		size,
 		linesize,
 		text="",
 		antialias=0,
@@ -28,8 +28,11 @@
 		background=None
 	)
 		Creates a list of 'Image' surface objects wrapped with the
-		given 'rect' argument. If rect's height is 0, will only
+		given 'size' argument. If size's height is 0, will only
 		restrict the width.
+
+		Returns a list (list), total height (int), and excess
+		text (string).
 
 	.flush()
 		Clears the font cache, allowing new 'Font' objects with
@@ -37,50 +40,126 @@
 '''
 import pygame
 
-class this:
-	pass
-
 cache = {}
 
 def load(senpai):
-	def get(name, size, bold=False, italic=False):
-		global cache
-		tuple = (name, size, bold, italic)
+	class this:
+		__new__ = senpai.__new__
 
-		if tuple not in cache:
-			cache[tuple] = pygame.font.SysFont(
-				name,
-				size,
-				bold,
-				italic
-			)
+		def get(name, size, bold=False, italic=False):
+			global cache
+			tuple = (name, size, bold, italic)
 
-		return cache[tuple]
+			if tuple not in cache:
+				cache[tuple] = pygame.font.SysFont(
+					name,
+					size,
+					bold,
+					italic
+				)
 
-	def draw(
-		surface,
-		font,
-		rect,
-		text="",
-		antialias=0,
-		color=(0, 0, 0),
-		background=None,
-		align=(0, 0)
-	):
-		rect = pygame.Rect(rect)
+			return cache[tuple]
 
-		if rect.width > 0:
+		def draw(
+			surface,
+			font,
+			rect,
+			text="",
+			antialias=0,
+			color=(0, 0, 0),
+			background=None,
+			align=(0, 0)
+		):
+			rect = pygame.Rect(rect)
+
+			if rect.width > 0:
+				res = []
+				y = rect.top
+				linesize = font.get_linesize()
+				center = align[1] > 0 and align[1] < 1
+
+				while text:
+					i = 0 # Text length.
+					s = None # Last whitespace seen.
+
+					if rect.height > 0 and y > rect.bottom:
+						break
+
+					# Get length of text relative to width.
+					while i < len(text):
+						i += 1
+
+						if text[i-1].isspace():
+							s = i
+
+						if font.size(text[:i+1])[0] >= rect.width:
+							# Max width reached.
+							if s and (i < len(text) or not text[i].isspace()):
+								# Move back to the last whole word.
+								i = s
+
+							break
+
+					image = font.render(
+						text[:i],
+						antialias,
+						color,
+						background
+					)
+
+					if center:
+						res.append(image)
+					else:
+						image_rect = image.get_rect()
+
+						# Draw on surface.
+						surface.blit(image, (
+							rect.x + align[0]*(rect.width - image_rect.width),
+							y + align[1]*(rect.height - y*2 - image_rect.height)
+						))
+
+					text = text[i:]
+					y += linesize
+
+				n = len(res)
+				if n > 0:
+					w = y-rect.top-(linesize-res[n-1].get_rect().height)
+					y = rect.top
+
+					for image in res:
+						image_rect = image.get_rect()
+
+						surface.blit(image, (
+							rect.x + align[0]*(rect.width - image_rect.width),
+							y + align[1]*(rect.height - w)
+						))
+
+						y += linesize
+
+				return text
+
+			surface.blit(font.render(
+				text,
+				antialias,
+				color,
+				background
+			), rect)
+
+		def wrap(
+			font,
+			size,
+			linesize,
+			text="",
+			antialias=0,
+			color=(0, 0, 0),
+			background=None
+		):
 			res = []
-			y = rect.top
-			linesize = font.get_linesize()
-			center = align[1] > 0 and align[1] < 1
+			y = 0
 
 			while text:
 				i = 0 # Text length.
 				s = None # Last whitespace seen.
-
-				if rect.height > 0 and y > rect.bottom:
-					break
 
 				# Get length of text relative to width.
 				while i < len(text):
@@ -89,7 +168,7 @@ def load(senpai):
 					if text[i-1].isspace():
 						s = i
 
-					if font.size(text[:i+1])[0] >= rect.width:
+					if font.size(text[:i+1])[0] >= size[0]:
 						# Max width reached.
 						if s and (i < len(text) or not text[i].isspace()):
 							# Move back to the last whole word.
@@ -97,108 +176,25 @@ def load(senpai):
 
 						break
 
-				image = font.render(
+				res.append(font.render(
 					text[:i],
 					antialias,
 					color,
 					background
-				)
-
-				if center:
-					res.append(image)
-				else:
-					image_rect = image.get_rect()
-
-					# Draw on surface.
-					surface.blit(image, (
-						rect.x + align[0]*(rect.width - image_rect.width),
-						y + align[1]*(rect.height - y*2 - image_rect.height)
-					))
+				))
 
 				text = text[i:]
-				y += linesize
 
-			n = len(res)
-			if n > 0:
-				w = y-rect.top-(linesize-res[n-1].get_rect().height)
-				y = rect.top
-
-				for image in res:
-					image_rect = image.get_rect()
-
-					surface.blit(image, (
-						rect.x + align[0]*(rect.width - image_rect.width),
-						y + align[1]*(rect.height - w)
-					))
-
-					y += linesize
-
-			return text
-
-		surface.blit(font.render(
-			text,
-			antialias,
-			color,
-			background
-		), rect)
-
-	def wrap(
-		font,
-		rect,
-		linesize,
-		text="",
-		antialias=0,
-		color=(0, 0, 0),
-		background=None
-	):
-		rect = pygame.Rect((0, 0, rect[0], rect[1]))
-
-		res = []
-		y = 0
-
-		while text:
-			i = 0 # Text length.
-			s = None # Last whitespace seen.
-
-			# Get length of text relative to width.
-			while i < len(text):
-				i += 1
-
-				if text[i-1].isspace():
-					s = i
-
-				if font.size(text[:i+1])[0] >= rect.width:
-					# Max width reached.
-					if s and (i < len(text) or not text[i].isspace()):
-						# Move back to the last whole word.
-						i = s
-
+				if size[1] > 0 and y + linesize > size[1]:
 					break
 
-			res.append(font.render(
-				text[:i],
-				antialias,
-				color,
-				background
-			))
+				y += linesize
 
-			text = text[i:]
+			return res, y, text
 
-			if rect.height > 0 and y + linesize > rect.bottom:
-				break
+		def flush():
+			global cache
 
-			y += linesize
-
-		return res, y, text
-
-	def flush():
-		global cache
-
-		cache = {}
-
-	this.get = get
-	this.draw = draw
-	this.wrap = wrap
-	this.flush = flush
+			cache = {}
 
 	return this
